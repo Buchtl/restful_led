86f0445cc1196dc15a6de3e952a084ef04331751
Chris <schauer@inf-schauer.de>
2023-12-28 17:56:03 +0100

    feat(): first draft colorpicker

--- led_rgb_frontend/src/ColorPicker.tsx
+++ led_rgb_frontend/src/ColorPicker.tsx
@@ -1,8 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
+import {ColorResult, SketchPicker} from 'react-color'
 
 const ColorPicker: React.FC = () => {
-    fetch('http://pi4:8080/ryb?red=0&yellow=0&blue=0').then(r => console.log(r.statusText))
-    return (<div>Colorpicker</div>);
+    //fetch('http://pi4:8080/ryb?red=0&yellow=0&blue=0').then(r => console.log(r.statusText))
+    let [color, setColor] = useState<ColorResult>({
+        hex: "#000000",
+        rgb: {r: 0, g: 0, b: 0, a: 1},
+        hsl: {h: 0, s: 0, l: 0, a: 0}
+    });
+
+    const handleColor = function (color: ColorResult) {
+        setColor(color)
+        let plain_hex = color.hex.replace("#", "");
+        fetch('http://pi4:8080/rgb?rgb=' + plain_hex).then(r => console.log(r.statusText))
+    }
+
+    return (<div><SketchPicker color={color.hsl} onChange={handleColor}/></div>);
 };
 
 export default ColorPicker;
