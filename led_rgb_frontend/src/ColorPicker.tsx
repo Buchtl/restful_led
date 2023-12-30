70f4e692379b70de17a098617a9226a050fae93a
Chris <schauer@inf-schauer.de>
2023-12-30 10:36:41 +0100

    feat() init colorpicker on load

--- led_rgb_frontend/src/ColorPicker.tsx
+++ led_rgb_frontend/src/ColorPicker.tsx
@@ -1,23 +1,41 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {ColorResult, SketchPicker} from 'react-color'
+
+const URL_BASE = 'http://pi4b:8080'
+const URL_RGB = URL_BASE + "/rgb"
 
 interface ColorPickerProps {
     initial_rgb: string;
 }
+
+function  generate_color_result_by_hex(hex: string) :ColorResult {
+    return {
+        hex: hex, rgb: {r: 0, g: 0, b: 0, a: 0}, hsl: {h: 0, s: 0, l: 0, a: 0}
+    }
+}
+
 const ColorPicker: React.FC<ColorPickerProps> = (props) => {
-    let [color, setColor] = useState<ColorResult>({
-        hex: "#000000",
-        rgb: {r: 0, g: 0, b: 0, a: 1},
-        hsl: {h: 0, s: 0, l: 0, a: 0}
-    });
+
+    let initial_color: ColorResult = generate_color_result_by_hex("#000000")
+
+    let [color, setColor] = useState<ColorResult>(initial_color);
+    const fetchColor = async (): Promise<ColorResult> => {
+        let response = await fetch(URL_RGB)
+        let rgb_str = await response.text()
+        return generate_color_result_by_hex(rgb_str.replace("0x", "#"))
+    }
+
+    useEffect(() => {
+        fetchColor().then(r => setColor(r))
+    }, []);
 
     const handleColor = function (color: ColorResult) {
         setColor(color)
         let plain_hex = color.hex.replace("#", "");
-        fetch('http://pi4b:8080/rgb/' + plain_hex).then(r => console.log(r.statusText))
+        fetch(URL_RGB + "/" + plain_hex).then(r => console.log(r.statusText))
     }
 
-    return (<div><SketchPicker color={color.hsl} onChange={handleColor}/></div>);
+    return (<div><SketchPicker color={color.hex} onChange={handleColor}/></div>);
 };
 
 export default ColorPicker;
