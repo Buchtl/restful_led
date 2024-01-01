6bfcaef7c37cfb77fe041de04afa29a7ec6a1cdb
Chris <schauer@inf-schauer.de>
2024-01-01 15:47:31 +0100

    refactor() test currentRgb

--- led_rgb/tests/test_led_pwm.py
+++ led_rgb/tests/test_led_pwm.py
@@ -5,7 +5,7 @@ from led_rgb.src.led_pwm import LedPwm
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_output(self):
+    def test_current_rgb(self):
         subject = LedPwm()
         self.assertEqual('0x000000', subject.get_rgb())
 
